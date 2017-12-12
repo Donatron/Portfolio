e0dc5de990c2a6dcff477793c84d40821c841ebc
Donatron <don.macarthur68@gmail.com>
2017-12-12 12:22:48 +1000

    updated json call and fucntionality
    added sections for treehouse badges and points to index.html

--- src/app.js
+++ src/app.js
@@ -1,7 +1,3 @@
-// const https = require('https');
-console.log("Hey dude");
-
-
 //   $('.overlay').hide();
 //
 //   // Show portfolio image overlays
@@ -17,12 +13,26 @@ $.getJSON({
   context: document.body
 }).done(function($response) {
 
+  // Generate Random Index Number
+  function getIndex($length) {
+    $index = Math.floor(Math.random() * $length) + 1;
+    return $index;
+  }
+
+  // Find 8 random Badges
+  for(let i=1; i<=8; i++) {
+    $index = getIndex($response.badges.length);
+    console.log('Donatron was awarded the ' + $response.badges[$index].name + ' Badge in the ' + $response.badges[$index].courses[0].title + ' course.');
+  }
+
   $values = Object.values($response.points);
   $keys = Object.keys($response.points);
 
   for($key in $keys) {
-
-      console.log("I have earned " + $values[$key] + " points in " + $keys[$key]);
+    // Filter out points less than 1000
+    if($values[$key] > 1000 && $keys[$key] != 'total') {
+      console.log("Donatron has earned " + $values[$key] + " points in " + $keys[$key]);
+    }
 
   }
 
