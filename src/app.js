9fea0bb0b36d0488e978132179af32664e0d3aee
Donatron <don.macarthur68@gmail.com>
2017-12-16 09:43:43 +1000

    updated app.js to feed treehouse points earned to html div

--- src/app.js
+++ src/app.js
@@ -8,23 +8,41 @@
 //   }
 // );
 
+function createBadgesHeadingHTML($total) {
+  let $html = '<p class="col-12 text-center">';
+
+  $html += 'These are some of the ' + $total + ' badges I have received for my achievements with ';
+  $html += '<a href="https://teamtreehouse.com" class="treehouse-link">Team Treehouse</a> so far</p>';
+
+  return $html;
+}
+
 function createBadgesHTML($badge, $name, $course, $url) {
-  let html = '<div class="card col-6 col-md-3 badges" style="width: 20rem;">';
+  let $html = '<div class="card col-6 col-md-3 badges" style="width: 20rem;">';
 
-  html += '<img class="card-img-top treehouse-badge" ';
-  html += 'src="' + $badge + '" ';
-  html += 'alt="Image for Treehouse "' + $name + '" badge">';
-  html += '<div class="card-body text-center">';
-  html += '<h4 class="card-title">Badge Name:</h4>';
-  html += '<p>' + $name + '</p>';
-  html += '<h4>Course:</h4>';
-  html += '<p>' + $course + '</p>';
-  html += '</div>';
-  html += '<a href="' + $url + '" target="_blank"';
-  html += 'class="btn btn-outline-info btn-badges mb-5">View Course</a>';
-  html += '</div>';
+  $html += '<img class="card-img-top treehouse-badge" ';
+  $html += 'src="' + $badge + '" ';
+  $html += 'alt="Image for Treehouse "' + $name + '" badge">';
+  $html += '<div class="card-body text-center">';
+  $html += '<h4 class="card-title">Badge Name:</h4>';
+  $html += '<p>' + $name + '</p>';
+  $html += '<h4>Course:</h4>';
+  $html += '<p>' + $course + '</p>';
+  $html += '</div>';
+  $html += '<a href="' + $url + '" target="_blank"';
+  $html += 'class="btn btn-outline-info btn-badges mb-5">View Course</a>';
+  $html += '</div>';
 
-  return html;
+  return $html;
+}
+
+function createPointsHTML($language, $points) {
+  $html = '<div class="points-container text-center">';
+  $html += '<h4>' + $language + '</h4>';
+  $html += '<p>' + $points + ' points</p>';
+  $html += '</div>';
+
+  return $html;
 }
 
 $.getJSON({
@@ -32,16 +50,23 @@ $.getJSON({
   context: document.body
 }).done(function($response) {
 
-  // Generate Random Index Number
+  // Find total number of badges for output to section description
+  $totalBadges = $response.badges.length;
+  $headingHTML = createBadgesHeadingHTML($totalBadges);
+
+  $('#badges-heading').append($headingHTML);
+
+  // Generate Random Index Number for displaying random badges
   function getIndex($length) {
     $index = Math.floor(Math.random() * $length) + 1;
     return $index;
   }
 
-  // Find 8 random Badges
+  // Find and display 8 random Badges
   for(let i=1; i<=8; i++) {
     $index = getIndex($response.badges.length);
-    // console.log('Donatron was awarded the ' + $response.badges[$index].name + ' Badge in the ' + $response.badges[$index].courses[0].title + ' course.');
+
+    // Declare variables for badge properties
     $badgeImage = $response.badges[$index].icon_url;
     $badgeName = $response.badges[$index].name;
     $course = $response.badges[$index].courses[0].title;
@@ -49,20 +74,35 @@ $.getJSON({
 
     $badgeHTML = createBadgesHTML($badgeImage, $badgeName, $course, $courseURL);
 
-    console.log($badgeHTML);
+    //Append badges to HTML div
     $('#badges').append($badgeHTML);
   }
 
+  // Declare key/value pairs for Treehouse points earned
   $values = Object.values($response.points);
   $keys = Object.keys($response.points);
+  $totalPoints = 0;
 
   for($key in $keys) {
+
     // Filter out points less than 1000
     if($values[$key] > 1000 && $keys[$key] != 'total') {
-      console.log("Donatron has earned " + $values[$key] + " points in " + $keys[$key]);
+
+      // Declare points variables
+      $language = $keys[$key];
+      $points = $values[$key];
+
+      // Append points earned details to HTML div
+      $pointsHTML = createPointsHTML($language, $points);
+      $('#points').append($pointsHTML);
+
+      // Calculate total points earned
+      $totalPoints += $points;
+
     }
 
   }
-
+  
+  console.log('Total points are: ' + $totalPoints);
 
 });
