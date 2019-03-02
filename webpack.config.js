63dbfa1c98d1d154d7d4360f782fda93eaf058de
Donatron <don.macarthur68@gmail.com>
2019-03-02 15:39:42 +1000

    added jump.js package

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: {
+    main: "./src/index.js"
+  },
+  output: {
+    filename: "[name]-bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: ""
+  },
+  mode: "development",
+  devServer: {
+    contentBase: "dist",
+    index: "index.html",
+    port: 9000,
+    overlay: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].html"
+            }
+          },
+          {
+            loader: "extract-loader"
+          },
+          {
+            loader: "html-loader",
+            options: {
+              attr: ["img:src"]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|svg|gif|ico)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "assets/[folder]/[name].[ext]"
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [new CleanWebpackPlugin("dist")]
+};
